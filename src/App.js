bfa197793419a710040846ba89cb4609db139a1a
nleake <6044084+nleake@users.noreply.github.com>
2025-02-12 15:32:48 -0500

    initial commit

---
+++ src/App.js
@@ -0,0 +1,9 @@
+import Die from "./Die";
+
+function App() {
+    return (<div>
+            <Die face='five'/>
+    </div>)
+}
+
+export default App;
---
+++ src/Die.js
@@ -0,0 +1,9 @@
+function Die({face}) {
+
+    
+    return <div>
+        <i className={`fas fa-dice-${face}`}></i>
+    </div>
+}
+
+export default Die;
