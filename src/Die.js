3fcd83c774823e0b9db0312ad5398b4d8702f514
nleake <6044084+nleake@users.noreply.github.com>
2025-02-13 15:41:53 -0500

    add dice shaking animation, disable button on click, but introducing bug on state...

--- src/Die.js
+++ src/Die.js
@@ -1,10 +1,10 @@
 import "./Die.css";
 
-function Die({face}) {
+function Die({face, rolling}) {
 
     
     return <div>
-        <i className={`Die fas fa-dice-${face}`}></i>
+        <i className={`Die fas fa-dice-${face} ${rolling && 'wobble'}`}></i>
     </div>
 }
 
--- src/RollDice.js
+++ src/RollDice.js
@@ -1,29 +1,40 @@
-import { useState } from "react";
-import Die from "./Die";
-import './RollDice.css';
+    import { useState } from "react";
+    import Die from "./Die";
+    import './RollDice.css';
 
-function RollDice() {
-    const [roll, setRoll] = useState({
-        die1: 'one',
-        die2: 'one',
-        rolling: false,
-    });
+    function RollDice() {
+        const [roll, setRoll] = useState({
+            die1: 'one',
+            die2: 'one',
+            rolling: false,
+        });
 
-    const newRoll = () => {
-        const sides = ['one', 'two', 'three', 'four', 'five', 'six'];
-        const newDie1 = sides[Math.floor(Math.random() * sides.length)];
-        const newDie2 = sides[Math.floor(Math.random() * sides.length)];
 
-        setRoll({ die1: newDie1, die2: newDie2 });
+//TODO: FIX THE FACT THAT THE STATE ISNT STAYING UPDATED.....
+        const newRoll = () => {
+            //set sides of the die
+            const sides = ['one', 'two', 'three', 'four', 'five', 'six'];
+            //pick new rolls
+            const newDie1 = sides[Math.floor(Math.random() * sides.length)];
+            const newDie2 = sides[Math.floor(Math.random() * sides.length)];
+            //set state with new rolls
+            setRoll({ die1: newDie1, die2: newDie2, rolling: true });
+            //wait one second, then set rolling to false
+            setTimeout(() => {
+                setRoll({...roll, rolling: false})
+            }, 1000)
+
+        }
+
+        return <div className="RollDice">
+            <div className="dice">
+                <Die face={roll.die1} rolling={roll.rolling}/>
+                <Die face={roll.die2} rolling={roll.rolling}/>
+            </div>
+            <button onClick={newRoll} disabled={roll.rolling}>
+                {roll.rolling ? 'Rolling...' : 'Roll Dice'}
+            </button>
+        </div>
     }
 
-    return <div className="RollDice">
-        <div className="dice">
-            <Die face={roll.die1} />
-            <Die face={roll.die2} />
-        </div>
-        <button onClick={newRoll}>Roll Dice</button>
-    </div>
-}
-
-export default RollDice;
+    export default RollDice;
